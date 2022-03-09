298a1196f0d95a0d52b2e81e7c0a8745a33cf885
Emilia <emiliaprzybylek@tutamail.com>
2022-03-09 15:11:31 +0100

    script added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const drinkSearch = document.querySelector('.search');
+const drinkList = document.querySelectorAll('li');
+
+const searchEngine = e => {
+
+    const text = e.target.value.toLowerCase();
+    console.log(text);
+
+    drinkList.forEach(el => {
+        const task = el.textContent;
+
+        if(task.toLowerCase().indexOf(text) !== -1) {
+            el.style.display = 'block'
+        } else {
+            el.style.display = 'none'
+        };
+    })
+}
+
+drinkSearch.addEventListener('keyup', searchEngine)
+
